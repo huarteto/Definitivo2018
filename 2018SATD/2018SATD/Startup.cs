3c4e89250480700f282a001ef0906cd4299918aa
Alvaro Huarte <alvaroSATD@hotmail.com>
2018-11-30 18:49:57 +0100

    Confirmar todo

---
+++ 2018SATD/2018SATD/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_2018SATD.Startup))]
+
+namespace _2018SATD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
